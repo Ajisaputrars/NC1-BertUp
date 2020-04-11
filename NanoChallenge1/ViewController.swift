654effc0e051053f5ecff516179974217c47f824
Aji Saputra Raka Siwi <ajisaputrars@gmail.com>
2020-04-11 12:29:11 +0700

    Initial Commit

---
+++ NanoChallenge1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NanoChallenge1
+//
+//  Created by Aji Saputra Raka Siwi on 11/04/20.
+//  Copyright © 2020 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

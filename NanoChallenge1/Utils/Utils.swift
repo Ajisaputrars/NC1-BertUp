62042a9ea657900b44d7c9d30e6288f5faa6422a
Aji Saputra Raka Siwi <ajisaputrars@gmail.com>
2020-04-11 14:57:10 +0700

    utils(temperature converter): add kelvin to celcius module

---
+++ NanoChallenge1/Utils/Utils.swift
@@ -0,0 +1,15 @@
+//
+//  Utils.swift
+//  NanoChallenge1
+//
+//  Created by Aji Saputra Raka Siwi on 11/04/20.
+//  Copyright © 2020 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import Foundation
+
+class Utils {
+    static func kelvinToCelcius(_ temp: Double) -> Int{
+        return Int((round(temp)) - 273)
+    }
+}

c221f2a6ffff1543ef6ab04370d56c7138116450
Aji Saputra Raka Siwi <ajisaputrars@gmail.com>
2020-04-12 22:14:24 +0700

    chore(timestatus module): add timestatus module to get time status

--- NanoChallenge1/Utils/Utils.swift
+++ NanoChallenge1/Utils/Utils.swift
@@ -16,4 +16,23 @@ class Utils {
     static func getDate() -> (hour: Int, minute: Int, second: Int){
         return (Calendar.current.component(.hour, from: Date()), Calendar.current.component(.minute, from: Date()), Calendar.current.component(.second, from: Date()))
     }
+    
+    static func getTimeStatus() -> TimeStatus{        
+        switch getDate().hour {
+        case 0...4:
+            return .EarlyMorning
+        case 5...8:
+            return .Morning
+        case 9...11:
+            return .LateMorning
+        case 12...16:
+            return .Afternoon
+        case 17...19:
+            return .Evening
+        case 20...22:
+            return .Night
+        default:
+            return .Midnight
+        }
+    }
 }

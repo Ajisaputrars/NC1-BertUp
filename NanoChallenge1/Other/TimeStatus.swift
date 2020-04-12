dd24a38d95e55f5cb9b2757ad456aac4bb747475
Aji Saputra Raka Siwi <ajisaputrars@gmail.com>
2020-04-12 22:13:55 +0700

    chore(timestatus): move timestatus enum to a separate file

---
+++ NanoChallenge1/Other/TimeStatus.swift
@@ -0,0 +1,19 @@
+//
+//  TimeStatus.swift
+//  NanoChallenge1
+//
+//  Created by Aji Saputra Raka Siwi on 12/04/20.
+//  Copyright © 2020 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import Foundation
+
+enum TimeStatus {
+    case Morning
+    case LateMorning
+    case Afternoon
+    case Evening
+    case Night
+    case Midnight
+    case EarlyMorning
+}

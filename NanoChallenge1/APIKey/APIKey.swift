f91df3f93e5e2767f8ca385001b4b4dd7ede5765
Aji Saputra Raka Siwi <ajisaputrars@gmail.com>
2020-04-13 21:58:34 +0700

    feat(weather): preparing weather feature

---
+++ NanoChallenge1/APIKey/APIKey.swift
@@ -0,0 +1,14 @@
+//
+//  APIKey.swift
+//  NanoChallenge1
+//
+//  Created by Aji Saputra Raka Siwi on 13/04/20.
+//  Copyright © 2020 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import Foundation
+
+class APIKey {
+    static let API_KEY = ""
+    static let LOCATION = ""
+}
